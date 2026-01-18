8b0927ee2c5b02cee143afb887fb6807fd761547
Vicki18 <2403218G@student.tp.edu.sg>
2026-01-18 22:29:43 +0800

    Final Project Application Submitted

--- OnlineBookStore/Data/OnlineBookStoreUser.cs
+++ OnlineBookStore/Data/OnlineBookStoreUser.cs
@@ -2,13 +2,13 @@
 
 namespace OnlineBookStore.Data
 {
-    
+
     public class OnlineBookStoreUser : IdentityUser
     {
         public string? FirstName { get; set; }
         public string? LastName { get; set; }
 
-        
+
         public string? Address { get; set; }
 
         public string? DisplayName { get; set; }
